c229055064f48779034c3d83f69b7b1d00a44ad1
haider647 <khokharcreation88@gmail.com>
2026-02-18 12:15:37 +0500

    fix asyncio for Railway

--- bot.py
+++ bot.py
@@ -81,5 +81,10 @@ async def main():
 
 import asyncio
 
-loop = asyncio.get_event_loop()
+try:
+    loop = asyncio.get_event_loop()
+except RuntimeError:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
 loop.run_until_complete(main())
